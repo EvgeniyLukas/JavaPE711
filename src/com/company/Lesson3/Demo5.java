f9feefaa60b599efa8019328d6e98047b7b87af2
ЛукашикЕвгенийАлекса <46prigram@gmail.com>
2020-06-24 17:24:59 +0300

    Foxed DEADLOCK exception

--- src/com/company/Lesson3/Demo5.java
+++ src/com/company/Lesson3/Demo5.java
@@ -5,34 +5,22 @@ public class Demo5 {
     static class Friend {
         private String name;
 
-
         public Friend(String name) {
             this.name = name;
         }
-
         public String getName() {
             return name;
         }
-
         public void setName(String name) {
             this.name = name;
         }
-
         public synchronized void seyHi(Friend friend) {
             System.out.println("Hi " + this.name + " " + friend.getName());
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            hiBack(friend);
+            friend.hiBack(friend);
         }
-
         public synchronized void hiBack(Friend friend) {
             System.out.println("Back " + this.name + " " + friend.getName());
-            seyHi(friend);
         }
-
         public static void main(String[] args) {
             Friend petr = new Friend("Петр");
             Friend ivan = new Friend("Иван");
